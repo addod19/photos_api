e98832780ca6fb94b0ec607ec11598c62a826e99
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2020-06-22 13:26:43 +0000

    install jwt and device configration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         :jwt_authenticatable,
-         jwt_revocation_strategy: JWTBlacklist
+        #  :jwt_authenticatable
+        #  jwt_revocation_strategy: JWTBlacklist
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,6 +18,13 @@ Devise.setup do |config|
 
   config.jwt do |jwt|
     jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
+    jwt.dispatch_requests = [
+      ['POST', %r{^/login$}]
+    ]
+    jwt.revocation_requests = [
+      ['DELETE', %r{^/logout$}]
+    ]
+    jwt.expiration_time = 1.day.to_i
   end
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
